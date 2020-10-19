f49d32103159620a3eb8e4e432d5205ae9386114
Masfik Alam <masfikalamfrp@gmail.com>
2020-10-20 00:32:27 +0600

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Dynamic-Bank
+
+This is a simple JavaScript calculation project. It's a bank account where user can see his bank balance, deposit money to the balance and withdraw money from the balance. Negetive cases are handled with conditions.
--- bank.js
+++ bank.js
@@ -6,22 +6,20 @@ loginBtn.addEventListener('click', function() {
     document.getElementById('transaction').style.display = "block";
 })
 
-
-
 // deposit calculation
 const depoBtn = document.getElementById('deposit');
 depoBtn.addEventListener('click', function() {
     let balValue = parseFloat(document.getElementById('balance-value').innerText);
     let depoValue = parseFloat(document.getElementById('deposit-value').innerText);
     let depoAmount = parseFloat(document.getElementById('depo-amount').value);
-    let depoAddition = depoAmount + depoValue;
-    let balAddition = balValue + depoAmount;
-    document.getElementById('balance-value').innerText = balAddition;
-    document.getElementById('deposit-value').innerText = depoAddition;
-    document.getElementById('depo-amount').value = "";
+    if (depoAmount > 0) {
+        let depoAddition = depoAmount + depoValue;
+        let balAddition = balValue + depoAmount;
+        document.getElementById('balance-value').innerText = balAddition;
+        document.getElementById('deposit-value').innerText = depoAddition;
+        document.getElementById('depo-amount').value = "";
+    }
 })
-
-
 
 // withdraw calculation
 const withBtn = document.getElementById('withdraw');
@@ -29,9 +27,11 @@ withBtn.addEventListener('click', function() {
     let balValue = parseFloat(document.getElementById('balance-value').innerText);
     let withValue = parseFloat(document.getElementById('withdraw-value').innerText);
     let withAmount = parseFloat(document.getElementById('with-amount').value);
-    let withAddition = withAmount + withValue;
-    let balAddition = balValue - withAmount;
-    document.getElementById('balance-value').innerText = balAddition;
-    document.getElementById('withdraw-value').innerText = withAddition;
-    document.getElementById('with-amount').value = "";
+    if (withAmount > 0 && withAmount < balValue) {
+        let withAddition = withAmount + withValue;
+        let balAddition = balValue - withAmount;
+        document.getElementById('balance-value').innerText = balAddition;
+        document.getElementById('withdraw-value').innerText = withAddition;
+        document.getElementById('with-amount').value = "";
+    }
 })
